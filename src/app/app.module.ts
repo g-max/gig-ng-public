f3329e4848291dec535704b009bcd9c27420befc
Maksym Husakov <maksym.husakov@acropolium.com>
2020-08-17 23:25:06 +0300

    added stub for http interaction

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { GeneratorComponent } from './components/generator/generator.component';
 import { PaymentsComponent } from './components/payments/payments.component';
 import { CodeComponent } from './components/code/code.component';
+
 import { LettersOnlyDirective } from './directives/letters-only.directive';
+
+
 
 @NgModule({
   declarations: [
@@ -20,7 +25,8 @@ import { LettersOnlyDirective } from './directives/letters-only.directive';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
